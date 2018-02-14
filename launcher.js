52a396e039af810cc7592c79de1053132c7d6a7f
Gunnar Gabrielson <gunnar.gabrielson@tcnp3.com>
2018-02-14 10:33:28 -0700

    added comment

--- launcher.js
+++ launcher.js
@@ -4,6 +4,9 @@ const path = require('path');
 const appDir = path.dirname(require.main.filename);
 const entrypoint = `${appDir}/public/index.html`
 
+// Bazel doesn't seem to output to stdout so this WORKS but you don't get
+// any feedback in the terminal as to whats happening. Probably this would
+// work better if written as as shell script, but I lack the expertise.
 exec(`bazel run :parcel ${entrypoint}`, (err, stdout, stderr) => {
   if (err) {
     console.log('Error running the thingy...')
